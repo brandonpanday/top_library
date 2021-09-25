2904334fcd631b851f405e7a4dece941add756ef
Brandon Panday <brandongpanday@gmail.com>
2021-09-24 22:54:43 -0400

    Added display cards

--- js.js
+++ js.js
@@ -18,20 +18,42 @@ function addBookToLibrary() {
     bookAuthor = document.getElementById('author').value;
     bookPages = parseInt(document.getElementById('pages').value);
     
-    bookOne = new Book(bookTitle, bookAuthor, bookPages);
+    bookNew = new Book(bookTitle, bookAuthor, bookPages);
 
-    myLibrary.push(bookOne);
+    myLibrary.push(bookNew);
     
     console.log(myLibrary);
 }
 
+// Loop through array and add books
+function displayBooks() {
+    let container = document.getElementById('container');   
+
+    myLibrary.forEach(element => {
+        console.log(`Title: ${element.title} Author: ${element.author} Pages: ${element.pages} added.`);
+        let newBook = document.createElement('div');
+        let cardTitle = document.createElement('p');
+        cardTitle.innerHTML += element.title;
+        let cardAuthor = document.createElement('p');
+        cardAuthor.innerHTML += element.author;
+        let cardPages = document.createElement('p');
+        cardPages.innerHTML += element.pages;
+        newBook.className = 'bookCard';
+        newBook.append(cardTitle, cardAuthor, cardPages); 
+        container.appendChild(newBook);
+
+    })
+}
+
+
 // Add books to Library
 function presetBooks() {
-    bookTwo = new Book("Book", "Author", 100);
+    bookOne = new Book("Book One", "Author 1", 101);
+    bookTwo = new Book("Book Two", "Author 2", 102);
+    bookThree = new Book("Book Three", "Author 3", 103);
+    myLibrary.push(bookOne);
     myLibrary.push(bookTwo);
-    myLibrary.push(bookTwo);
-    myLibrary.push(bookTwo);
+    myLibrary.push(bookThree);
     console.log(myLibrary);
 }
 window.onload = presetBooks();
-
